b00ed85f26f83f2e1f0a6273a679de988d2281b1
Fahim Abrar <fahim.abrar101@gmail.com>
2022-07-03 16:40:35 +0600

    7/3/2022 4:40pm

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -11,14 +12,37 @@ import 'package:unnayan/Services/notification_service.dart';
 import 'Components/badge_model.dart';
 import 'LoginPage/view/loginpage_view.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
 Future<void> mainIni() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp().whenComplete(() {
     print("completed");
   });
-
   await NotificationService().iniNotification();
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
 }
 
